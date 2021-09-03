e4eb677524c88dcb75653cab3c05fb736622e8fc
Ryan Burns <ryankburns@gmail.com>
2021-09-02 19:13:35 -0700

    defined db tables

--- db/migrate/20210902141314_create_farms.rb
+++ db/migrate/20210902141314_create_farms.rb
@@ -1,6 +1,9 @@
 class CreateFarms < ActiveRecord::Migration[6.1]
   def change
     create_table :farms do |t|
+      t.string :name
+      t.string :location
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20210903015234_create_crops.rb
+++ db/migrate/20210903015234_create_crops.rb
@@ -1,6 +1,9 @@
 class CreateCrops < ActiveRecord::Migration[6.1]
   def change
     create_table :crops do |t|
+      t.string :strain_name
+      t.datetime :harvest_date
+      
 
       t.timestamps
     end
