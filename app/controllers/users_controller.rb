14de7a98add0f477e76287e1f42bd340493158f8
Ryan Burns <ryankburns@gmail.com>
2021-09-02 20:27:41 -0700

    added user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+
+    def index
+        @users = User.all
+        render json: users, only [:id, :username]
+    end
+
+    def show
+        user = User.find_by(params[:id])
+        render json: user, only: [:id, :username]
+    end
+
+    def create
+        user = User.new(user_params)
+            if user.save
+                session[:user_id] = user.id
+                render json: user, only: [:id, :username]
+            else
+                render json: user.errors.full_messages
+            end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :id, :password)
+    end
+
 end
