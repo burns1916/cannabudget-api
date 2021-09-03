9c3582b0515b8952b503fb7de977e13dae140314
Ryan Burns <ryankburns@gmail.com>
2021-09-03 07:11:28 -0700

    added current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    
+    def current_user
+        User.find_by(id: session[:id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,14 @@ class SessionsController < ApplicationController
                 sessions[:user_id] = user.id
                 render json: user
             else
-                render json: user.errors.full_messages
+                flash[:message] = "Login information was incorrect"
             end
     end
 
-    
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: "Please Login"
+    end
 end
