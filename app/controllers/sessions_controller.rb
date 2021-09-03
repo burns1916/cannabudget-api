c7cc716c713056aff0c8d4113dd80b342c913ebd
Ryan Burns <ryankburns@gmail.com>
2021-09-02 20:34:55 -0700

    added sessions login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+            if user && user.authenticate(params[:session][:password])
+                sessions[:user_id] = user.id
+                render json: user
+            else
+                render json: user.errors.full_messages
+            end
+    end
+
+    
 end
