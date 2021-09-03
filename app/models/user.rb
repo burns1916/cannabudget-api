c4d01f4c2fd364848099b402d487de7e0c2fb01f
Ryan Burns <ryankburns@gmail.com>
2021-09-03 07:20:44 -0700

    added login/logout routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :farms
     has_many :crops, through :farms
+
+    validates :username, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :crops
   resources :farms
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get '/signup' => 'users#new'
+  post 'signup' => 'users=>create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
 end
