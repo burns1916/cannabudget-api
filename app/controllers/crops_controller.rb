84e8b7a20b83b9ed39fecb464f32ccd19a8c2000
Ryan Burns <ryankburns@gmail.com>
2021-09-04 10:12:31 -0700

    CRUD methods added

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -1,2 +1,44 @@
 class CropsController < ApplicationController
+
+    def index
+        crops = Crops.all
+        render json: crops, include [:farm]
+    end
+
+    def show
+        crop = Crop.find_by(params[:id])
+        render json: crop, include [:farm]
+    end
+
+    def create
+        crop = Crop.new(crop_params)
+            if crop.save
+                render json: crop
+            else
+                render json: crop.errors.full_messages
+            end
+    end
+
+    def update
+        crop = Crop.find_by(params[:id])
+        crop.update(crop_params)
+            if crop.save
+                render json: crop
+            else
+                render json: crop.errors.full_messages
+            end
+    end
+
+    def destroy
+        crop = Crop.find_by(params[:id])
+        crop.destroy
+        render json: flash[:message] = "Crop Deleted"
+    end
+
+    private
+
+    def crop_params
+        params.require(:crop).permit(:farm_id, :strain_name, :harvest_date)
+    end
+
 end
--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,2 +1,43 @@
 class FarmsController < ApplicationController
+
+    def index
+        farms = Farms.all
+        render json: farms, include [:user]
+    end
+
+    def show
+        farm = Farm.find_by{params[:id])
+        render json: farm, include [:user]
+    end
+
+    def create
+        farm = Farm.new(current_user.farms.build(farm_params))
+            if farm.save
+                render json: farm
+            else
+                render json: farm.errors.full_messages
+            end
+    end
+
+    def destroy
+        farm = Farm.find_by(params[:id])
+        farm.destroy
+        render json: flash[:message] = "Farm Deleted"
+    end
+
+    def update
+        farm = Farm.find_by(params[:id])
+        farm.update(farm_params)
+            if farm.save
+                render json: farm
+            else
+                render json: farm.errors.full_massages
+            end
+    end
+
+    private
+
+    def farm_params
+        params.require(:farm).permit(:user_id, :name, :location)
+    end
 end
