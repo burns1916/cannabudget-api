357e086cffec4ab212622f7595b52f8f1a2c1d21
Ryan Burns <ryankburns@gmail.com>
2021-09-04 10:30:47 -0700

    fixed typo in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-
-
+    
     def index
         users = User.all
         render json: users, only [:id, :username]
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
   
   get '/signup' => 'users#new'
-  post 'signup' => 'users=>create'
+  post 'signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
