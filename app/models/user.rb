acf58cb7d03dbe74eaa23f6c51a496300b34ea6d
Ryan Burns <ryankburns@gmail.com>
2021-09-12 19:46:14 -0700

    fixe syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :farms
-    has_many :crops, through :farms
+    has_many :crops, through: :farms
 
     validates :username, uniqueness: { case_sensitive: false }
 end
