ad9f64097f3b772239d6887f154d77e75d4e5eca
Ryan Burns <ryankburns@gmail.com>
2021-09-14 19:22:49 -0700

    fixed cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins '*'
+     origins 'https://localhost:3000', 'https://cannabudget-api.herokuapp.com/'
 
      resource '*',
        headers: :any,
