66b33929e36dd53a785ede071a454101c71d46f9
Ryan Burns <ryankburns@gmail.com>
2021-09-15 16:43:53 -0700

    fixed syntax

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:session][:username])
             if user && user.authenticate(params[:session][:password])
-                sessions[:user_id] = user.id
+                session[:user_id] = user.id
                 render json: user, status: 200
             else
                 render json: {
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.permit(:username, :password)
     end
 
 end
